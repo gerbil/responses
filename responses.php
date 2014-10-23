339d55f864ad0de73f6763073f834495abcfabf1
gerbil <jurijs.kobecs@gmail.com>
2014-10-23 10:02:01 +0300

    responses

--- responses.php
+++ responses.php
@@ -116,59 +116,70 @@ if (isset($_FILES['file'])) {
             for ($c = 0; $c < $sizeOfChild; $c++) {
                 $xml = new XMLWriter();
                 $xml->openMemory();
-                $xml->startDocument();
-                $xml->startElementNS('aetgt', 'DeliverServiceResponse', 'http://www.tele2.com/DeliverServiceResponse/Canonical/v01');
-                $xml->writeAttribute('xmlns:ns2', 'http://www.tele2.com/DeliverServiceResponse/Canonical/v01');
-                $xml->writeAttribute('xmlns:ns', 'http://www.tele2.com/Common/MessageHeader/v02');
+                    $xml->startDocument();
+                        $xml->startElementNS('aetgt', 'DeliverServiceResponse', 'http://www.tele2.com/DeliverServiceResponse/Canonical/v01');
+                            $xml->writeAttribute('xmlns:ns2', 'http://www.tele2.com/DeliverServiceResponse/Canonical/v01');
+                            $xml->writeAttribute('xmlns:ns', 'http://www.tele2.com/Common/MessageHeader/v02');
 
-                $xml->startElementNS('ns2', 'Header', null);
-                $xml->writeElementNS('ns', 'MessageId', null, 'ProvidentM' . date('Y-m-d') . 'T' . date('H:i:s') . '+02:00');
-                $xml->writeElementNS('ns', 'ConversationId', null, $child[$c]->ConvID);
-                $xml->writeElementNS('ns', 'BusinessProcessId', null, $child[$c]->BusinessProcessId);
-                $xml->writeElementNS('ns', 'MessageTargetNamespace', null, 'http://new.webservice.namespace');
-                $xml->writeElementNS('ns', 'MessagePriority', null, '4');
-                $xml->writeElementNS('ns', 'Timestamp', null, date('Y-m-d') . 'T' . date('H:i:s') . 'Z');
-                $xml->writeElementNS('ns', 'Sender', null, $child[$c]->Sender);
-                $xml->writeElementNS('ns', 'Recipient', null, $child[$c]->Recipient);
-                $xml->writeElementNS('ns', 'LegalEntity', null, $child[$c]->LegalEntity);
-                $xml->writeElementNS('ns', 'PayloadEncoding', null, 'Plain');
-                $xml->endElement();
+                                $xml->startElementNS('ns2', 'Header', null);
+                                    $xml->writeElementNS('ns', 'MessageId', null, 'ProvidentM' . date('Y-m-d') . 'T' . date('H:i:s') . '+02:00');
+                                    $xml->writeElementNS('ns', 'ConversationId', null, $child[$c]->ConvID);
+                                    $xml->writeElementNS('ns', 'BusinessProcessId', null, $child[$c]->BusinessProcessId);
+                                    $xml->writeElementNS('ns', 'MessageTargetNamespace', null, 'http://new.webservice.namespace');
+                                    $xml->writeElementNS('ns', 'MessagePriority', null, '4');
+                                    $xml->writeElementNS('ns', 'Timestamp', null, date('Y-m-d') . 'T' . date('H:i:s') . 'Z');
+                                    $xml->writeElementNS('ns', 'Sender', null, $child[$c]->Sender);
+                                    $xml->writeElementNS('ns', 'Recipient', null, $child[$c]->Recipient);
+                                    $xml->writeElementNS('ns', 'LegalEntity', null, $child[$c]->LegalEntity);
+                                    $xml->writeElementNS('ns', 'PayloadEncoding', null, 'Plain');
+                                $xml->endElement();
 
-                $xml->startElementNS('ns2', 'Body', null);
-                $xml->startElementNS('ns2', 'ServiceOrderStatus', null);
-                $xml->writeElementNS('ns2', 'ServiceOrderID', null, $child[$c]->TXNID);
-                $xml->writeElementNS('ns2', 'StatusCode', null, $child[$c]->StatusCode);
-                $xml->writeElementNS('ns2', 'StatusReason', null, $child[$c]->StatusReason);
+                                $xml->startElementNS('ns2', 'Body', null);
+                                    $xml->startElementNS('ns2', 'ServiceOrderStatus', null);
+                                        $xml->writeElementNS('ns2', 'ServiceOrderID', null, $child[$c]->TXNID);
+                                        $xml->writeElementNS('ns2', 'StatusCode', null, $child[$c]->StatusCode);
+                                        $xml->writeElementNS('ns2', 'StatusReason', null, $child[$c]->StatusReason);
 
-                for ($i = 0; $i < sizeof($child[$c]->SOInstanceResponses); $i++) {
-                    $xml->startElementNS('aetgt', 'ServiceInstanceOrderStatus', null);
-                    $xml->writeElementNS('ns2', 'ServiceInstanceOrderID', null, $child[$c]->SOInstanceResponses[$i]->ServiceInstanceOrderID);
-                    $xml->writeElementNS('ns2', 'ServiceSpecificationName', null, $child[$c]->SOInstanceResponses[$i]->ServiceSpecificationName);
-                    $xml->writeElementNS('ns2', 'StatusCode', null, $child[$c]->SOInstanceResponses[$i]->StatusCode);
-                    $xml->writeElementNS('ns2', 'StatusReason', null, $child[$c]->SOInstanceResponses[$i]->StatusReason);
+                                for ($i = 0; $i < sizeof($child[$c]->SOInstanceResponses); $i++) {
+                                    $xml->startElementNS('aetgt', 'ServiceInstanceOrderStatus', null);
+                                        $xml->writeElementNS('ns2', 'ServiceInstanceOrderID', null, $child[$c]->SOInstanceResponses[$i]->ServiceInstanceOrderID);
+                                        $xml->writeElementNS('ns2', 'ServiceSpecificationName', null, $child[$c]->SOInstanceResponses[$i]->ServiceSpecificationName);
+                                        $xml->writeElementNS('ns2', 'StatusCode', null, $child[$c]->SOInstanceResponses[$i]->StatusCode);
+                                        $xml->writeElementNS('ns2', 'StatusReason', null, $child[$c]->SOInstanceResponses[$i]->StatusReason);
+                                    $xml->endElement();
+                                }
+
+                            $xml->endElement();
+                        $xml->endElement();
                     $xml->endElement();
-                }
-
-                $xml->endElement();
-                $xml->endElement();
-
-                $xml->endElement();
                 $xml = $xml->outputMemory();
+
                 $xml = str_replace('<?xml version="1.0"?>', '', $xml);
                 //var_dump($xml);
 
                 $result = '';
                 //system("curl --user SIT_Provident:SIT_Provident -H 'Content-Type: text/xml; charset=utf-8' --verbose -H 'SOAPAction:' -d '" . $xml . "' -X POST 'http://90.131.20.228:3401/ws/TIP_DeliverService_v01_NP_Provident.request.v01.webService:handleAsyncResponse' 2>uploads/errorlog.txt");
                 exec("curl --user SIT_Provident:SIT_Provident -H 'Content-Type: text/xml; charset=utf-8' --verbose -H 'SOAPAction:' -d '" . $xml . "' -X POST 'http://90.131.20.228:3401/ws/TIP_DeliverService_v01_NP_Provident.request.v01.webService:handleAsyncResponse'", $result, $returnCode);
-                //echo "<pre>";
-                //print_r($result);
-                var_dump($result);
 
-                $response = simplexml_load_string($result[0]);
-                print_r($response);
+                //PARSE RESPONSE
+                $clean_xml = str_replace('soapenv:', '', $result);
+                $clean_xml = str_replace('tns:', '', $clean_xml);
+                $clean_xml = str_replace('header:', '', $clean_xml);
+                $clean_xml = str_replace("<?xml version='1.0' encoding='utf-8'?>", '', $clean_xml);
+                $xml = simplexml_load_string($clean_xml[0]);
+                $statusCode = $xml->Body->CommonResponse->Body->StatusCode[0];
+                if ($statusCode == 0) {
+                    echo $child[$c]->TXNID." - OK<br/>";
+                } else {
+                    echo $child[$c]->TXNID." - NOT OK (".$statusCode.")<br/>";
+                };
+
             }
 
         }
+
+
+
 
         //SEND RESPONSE TO TIP
 
